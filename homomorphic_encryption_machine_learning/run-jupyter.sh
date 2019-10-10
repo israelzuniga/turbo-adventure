be48ca852d98aa25293db479d6b3534cc84df1dd
Israel Zúñiga de la Mora <6629+israelzuniga@users.noreply.github.com>
2019-10-10 10:06:03 -0500

    Changes to be committed: 	deleted:    SEALPython/requirements.txt 	deleted:    SEALPython/setup.py 	deleted:    SEALPython/wrapper.cpp 	modified:   run-jupyter.sh

--- homomorphic_encryption_machine_learning/run-jupyter.sh
+++ homomorphic_encryption_machine_learning/run-jupyter.sh
@@ -5,10 +5,10 @@ port=12345
 pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
 docker run -d \
   -p "${port}":"${port}" \
-  -v "$(pwd)":"/notebooks" \
+  -v "$(pwd)/PySEAL/SEALPythonNotebooks":"/notebooks" \
   -w "/notebooks" \
   --name pyseal-jupyter \
-  seal-python \
+  seal-save \
   jupyter notebook --ip=0.0.0.0 --port="${port}"  --allow-root --NotebookApp.token=''
 
 popd > /dev/null
