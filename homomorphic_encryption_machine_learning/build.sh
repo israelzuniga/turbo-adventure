7bf5e3f8d35c71985db3be137e1697430e937d31
Israel Zúñiga de la Mora <6629+israelzuniga@users.noreply.github.com>
2019-10-10 10:25:38 -0500

    new file:   build.sh

---
+++ homomorphic_encryption_machine_learning/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/Lab41/PySEAL.git
+cd PySEAL
+echo "pandas" >> SEALPython/requirements.txt
+echo "scikit-learn" >> SEALPython/requirements.txt
+echo "tensorflow" >> SEALPython/requirements.txt
+bash build-docker.sh
